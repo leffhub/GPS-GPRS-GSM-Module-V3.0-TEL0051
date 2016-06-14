8a0c11d67e38cb3299a6641cc276b682b599e60a
leffhub <laughs_dream11@163.com>
2016-06-14 18:38:52 +0800

    Integrate all the sample code on TEL0051 wiki
    Nothing new, all the same as you could found in its wiki.

---
+++ gps_gsm_sim908/Driving TEL0051 via AT mode/AT_GPS_TEL0051_Leonardo/AT_GPS_TEL0051_Leonardo.ino
@@ -0,0 +1,28 @@
+void setup() {
+  Serial.begin(9600); //initialize Serial(i.e. USB port)
+  Serial1.begin(9600); //initialize Serial1
+ 
+  //Init the driver pins for GSM function
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  //Output GSM Timing
+  digitalWrite(5, HIGH);
+  delay(1500);
+  digitalWrite(5, LOW);
+  digitalWrite(3, HIGH); //disable GSM TX、RX
+  digitalWrite(4, HIGH); //disable GPS TX、RX
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+  Serial.println("Input AT please...");
+}
+void loop() {
+  while (Serial1.available()) {
+    Serial.write(Serial1.read());
+  }
+  while (Serial.available()) {
+    Serial1.write(Serial.read());
+  }
+  delay(1);
+}
---
+++ gps_gsm_sim908/Driving TEL0051 via AT mode/AT_GSM_TEL0051_UNO/AT_GSM_TEL0051_UNO.ino
@@ -0,0 +1,14 @@
+void setup(){
+  //Init the driver pins for GSM function
+  pinMode(3, OUTPUT);
+  pinMode(4, OUTPUT);
+  pinMode(5, OUTPUT);
+  //Output GSM Timing
+  digitalWrite(5, HIGH);
+  delay(1500);
+  digitalWrite(5, LOW);
+}
+void loop(){
+  digitalWrite(3, LOW); //enable GSM TX、RX
+  digitalWrite(4, HIGH); //disable GPS TX、RX
+}
